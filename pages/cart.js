b03523b2c222318a844f4c514f1b11cbc6d652cb
unknown <maureecemarco@rocketmail.com>
2025-11-14 17:52:34 +0700

    fill in the cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -1,3 +1,19 @@
-exports.CartPage = class CartPage{
-    
+exports.CartPage = class CartPage {
+    constructor(cartClass) {
+        this.page = cartClass;
+        this.items = cartClass.getByText('Add to cart');
+        this.itemsRemove = cartClass.locator('button:has-text("Remove")');
+    }
+
+    async addToCart() {
+
+    }
+
+    async removeFromCart() {
+
+    }
+
+    async checkoutFromCart() {
+
+    }
 };
