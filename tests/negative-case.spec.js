1334e8114630a8d617151c7004177c05810ac60c
unknown <maureecemarco@rocketmail.com>
2025-11-17 15:29:50 +0700

    add negative-case.spec.js

--- tests/negative-case.spec.js
+++ tests/negative-case.spec.js
@@ -1,19 +1,21 @@
 const { expect, test } = require("@playwright/test");
-const {LoginPage} = require("../pages/login");
+const { LoginPage } = require("../pages/login");
+const { CartPage } = require("../pages/cart");
+const { CheckoutPage } = require("../pages/checkout");
 const { log } = require("console");
 
 
 test('Invalid password and username', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-    await loginPage.login("standard_user","tes123");
+    await loginPage.login("standard_user", "tes123");
 
     await expect(loginPage.errorMessage).toBeVisible();
     console.log("Error message password: ")
     console.log(await loginPage.errorMessage.textContent());
 
     await loginPage.usernameField.fill("");
-    await loginPage.login("user_standard","secret_sauce");
+    await loginPage.login("user_standard", "secret_sauce");
 
     await expect(loginPage.errorMessage).toBeVisible();
     console.log("Error message username: ")
@@ -30,4 +32,34 @@ test('Locked Out User', async ({ page }) => {
     await expect(loginPage.errorMessage).toBeVisible();
     console.log("Error message: ")
     console.log(await (loginPage.errorMessage).textContent());
+});
+
+test.only('Checkout Form contains empty field', async ({ page }) => {
+
+    const loginPage = new LoginPage(page);
+    const cartPage = new CartPage(page);
+    const webPageCheckout = new CheckoutPage(page);
+    
+    await loginPage.goto();
+    await loginPage.login("standard_user", "secret_sauce");
+
+    await cartPage.addToCart(1);
+    await cartPage.addToCart(1);
+    console.log("Success Add to Cart");
+
+    await cartPage.checkoutFromCart();
+
+    await expect(webPageCheckout.firstNameField).toBeVisible();
+
+    await webPageCheckout.fillCheckoutForm("", "Test", "15318");
+    await expect(webPageCheckout.errorMessage).toBeVisible();
+    console.log(await webPageCheckout.errorMessage.textContent());
+
+    await webPageCheckout.fillCheckoutForm("Test", "", "111111");
+    await expect(webPageCheckout.errorMessage).toBeVisible();
+    console.log(await webPageCheckout.errorMessage.textContent());
+
+    await webPageCheckout.fillCheckoutForm("Test", "Marco", "");
+    await expect(webPageCheckout.errorMessage).toBeVisible();
+    console.log(await webPageCheckout.errorMessage.textContent());
 });
